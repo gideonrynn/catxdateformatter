637c85afabcb6e65871a1e97efe8a4fa9d590d45
gideonrynn <sarah.c.dillard@gmail.com>
2021-09-28 23:11:54 -0500

    added all clear actions to one function, drafted date validation

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ function formatDates() {
     document.querySelector('#output').textContent = "";
     let originalDatesList = document.querySelector('#input').value;
     let newDatesList = "";
-    console.log("originalDatesList: " + "\n" + originalDatesList + " and the length is: " + originalDatesList.length);
+    // console.log("originalDatesList: " + "\n" + originalDatesList + " and the length is: " + originalDatesList.length);
 
     let sortedDatesList = originalDatesList.trim().split("\n");
-    console.log("sortedDatesList: " + "\n" + sortedDatesList + " and the length is: " + sortedDatesList.length);
+    // console.log("sortedDatesList: " + "\n" + sortedDatesList + " and the length is: " + sortedDatesList.length);
 
-    //construct date for each item in the array
+    //construct date for each item in the list
     sortedDatesList.forEach(date => {
 
         //set variables for M D YYYY
@@ -31,31 +31,38 @@ function formatDates() {
         let constructedDate = sortedYear + sortedMonth + sortedDay;
         // console.log("Date in YYYYMMDD format: " + constructedDate);
 
-        //add to final date that will be set in output
+        //add date validation
+        // let proposedDate = new Date(sortedYear,sortedMonth - 1,sortedDay - 1);
+        // console.log(proposedDate);
+        // if (proposedDate.getFullYear() == sortedYear && proposedDate.getMonth() == sortedMonth && proposedDate.getDate() == sortedDay) {
+        //   return "Success";
+        // } else {
+        // return "This is not a valid date: " + proposedDate;
+        //}
+
+        //add to final dates list that will appear in output
         newDatesList += constructedDate + "\n";
 
     })
 
     document.querySelector('#output').textContent = newDatesList;
     document.querySelector("#clear-right").removeAttribute("disabled");
+    document.querySelector("#clear-all").removeAttribute("disabled");
     console.log(document.querySelector('#output').innerHTML);
 
 }
 
-function clearOutput() {
-    document.querySelector('#output').innerHTML = "";
+function clearOutput(id) {
+
+    if(id === "clear-left") {
+        document.querySelector('#input').value = "";
+    }
+    if(id === "clear-right") {
+        document.querySelector('#output').innerHTML = "";
+        document.querySelector("#clear-right").setAttribute("disabled", true);
+    }
+    if(id === "clear-all") {
+        document.querySelector('#input').value = "";
+        document.querySelector('#output').innerHTML = "";
+    }
 }
-
-function clearOutputRight() {
-    document.querySelector('#output').innerHTML = "";
-    document.querySelector("#clear-right").setAttribute("disabled", true);
-}
-
-function clearInputLeft() {
-    document.querySelector('#input').value = "";
-}
-
-
-document.querySelector('#format').addEventListener("click", formatDates);
-document.querySelector('#clear-right').addEventListener("click", clearOutputRight);
-document.querySelector('#clear-left').addEventListener("click", clearInputLeft);
